b7c672210787825f7dd7d0c54d3071da91d8cbdb
Abdul Taher Khan <49634197+abdGhost@users.noreply.github.com>
2024-02-08 10:37:49 +0530

    added useState for setPlayerName

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,23 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [enterPlayerName, setEnterPlyerName] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+
+  function handleChange(event) {
+    setEnterPlyerName(event.target.value);
+  }
+
+  function handleClick() {
+    setSubmitted(true);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? enterPlayerName : "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChange} value={enterPlayerName} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
